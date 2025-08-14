fbd92b61f81b0f0b0239656345a21b0dfc4575b1
perez <jmperezg.srv@chileatiende.cl>
2025-08-14 14:20:21 -0400

    Add Modo Juego page with minimal layout for game space

---
+++ src/pages/modo-juego.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function ModoJuegoPage() {
+  return (
+    <>
+      <Head>
+        <title>Modo Juego</title>
+      </Head>
+      {/* Página intencionalmente sin widgets para maximizar el espacio del juego */}
+      <div className="p-2" />
+    </>
+  );
+}
